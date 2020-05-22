cbc7cd752f9abe41c9f6b368e16d096302f6e924
malvesbertoni <malvesbertoni@gmail.com>
2020-05-22 16:30:24 -0300

    Sketch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import imgSupervisor from './images/icon-supervisor.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main-wrapper">
+      <header>
+        <h1>Reliable, efficient delivery</h1>
+        <h1>Powered by Technology</h1>
+        <p>Our Artificial Intelligence powered tools use millions of project data points 
+            to ensure that your project is successful</p>
       </header>
+      <div className="cards-wrapper">
+        <div className="card">
+          <div className="card-header">
+            <h2>Supervisor</h2>
+            <p>Monitors activity to identify project roadblocks</p>
+          </div>
+          <div className="card-image">
+            <img src="imgSupervisor" alt="Supervisor"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
